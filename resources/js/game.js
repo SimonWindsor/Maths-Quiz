8665f45b08c8faf40568b229660e40c07eac620b
Simon Windsor <s3918370@student.rmit.edu.au>
2023-04-25 19:34:29 +1000

    Can't get sounds to work. Game now gives 10 questions and simple results display

--- resources/js/game.js
+++ resources/js/game.js
@@ -4,9 +4,15 @@ let correctAnswer;
 let maxNumber;
 let mathProblem = '';
 let answerInput = '';
+let totalQuestions = 0;
+let answerChecker = [];
+
 let plus = false;
 let takeAway = false;
 let friendsOfTen = false;
+
+// let correctSound = new Audio("powerup.mp3");
+// let incorrectSound = new Audio("lose.mp3");
 
 const gameSelector = document.getElementById('game-selector');
 const plusSelect = document.getElementById('adding');
@@ -15,7 +21,13 @@ const friendsOfTenSelect = document.getElementById('friends-of-ten');
 const easySelect = document.getElementById('easy');
 const mediumSelect = document.getElementById('medium');
 const hardSelect = document.getElementById('hard');
-const goButton = document.getElementById('go');
+const goButton = document.getElementById('go-button');
+const gameRadios = document.getElementsByName('game-mode');
+const difficultyRadios = document.getElementsByName('difficulty');
+
+const resultsPanel = document.getElementById('results-container');
+const results = document.getElementById('results');
+const okButton = document.getElementById('ok-button');
 
 const numberButtons = document.getElementsByClassName('number-btn');
 const enterButton = document.getElementById('enter');
@@ -23,7 +35,12 @@ const clearButton = document.getElementById('clear');
 const problemPanel = document.getElementById('problem');
 const inputPanel = document.getElementById('input');
 
+for(let i = 0; i < gameRadios.length; i++) {
+  gameRadios[i].addEventListener('click', disableDifficulty);
+}
+
 goButton.addEventListener('click', createGame);
+okButton.addEventListener('click', restart);
 
 // Add event listener for all number buttons
 for(let i = 0; i < numberButtons.length; i++)
@@ -33,9 +50,21 @@ for(let i = 0; i < numberButtons.length; i++)
 clearButton.addEventListener('click', clearInput);
 enterButton.addEventListener('click', checkAnswer);
 
+function disableDifficulty(event) {
+  if(event.target.value === 'friends-of-ten') {
+    for(let i = 0; i < difficultyRadios.length; i++)
+      difficultyRadios[i].classList.add('disabled');
+  }
+  else {
+    for(let i = 0; i < difficultyRadios.length; i++)
+      difficultyRadios[i].classList.remove('disabled');
+  }
+}
+
 // Creates a game on submission of "game-select" form
 function createGame() {
   gameSelector.classList.add('hide');
+  totalQuestions = 0;
   answerChecker = [];
 
   // Determine game mode
@@ -65,6 +94,7 @@ function createGame() {
 }
 
 function createProblem() {
+  totalQuestions++;
   if(!friendsOfTen) {
     firstNumber = Math.floor(Math.random() * (maxNumber + 1));
     secondNumber = Math.floor(Math.random() * (maxNumber + 1));
@@ -99,11 +129,41 @@ function clearInput() {
 
 function checkAnswer() {
   if(answerInput != '') {
-    if(Number(answerInput) === correctAnswer)
+    if(Number(answerInput) === correctAnswer) {
+      // correctSound.play();
       alert('Well Done!');
-    else
+      answerChecker.push(true);
+    }
+    else {
+      // incorrectSound.play();
       alert('Wrong, sorry.')
+      answerChecker.push(false);
+    }
     clearInput();
-    createProblem();
+
+    if(totalQuestions < 10)
+      createProblem();
+    else
+      showResults();
   }
+}
+
+function showResults() {
+  resultsPanel.classList.remove('hide');
+  results.innerHTML = answerChecker;
+}
+
+function restart() {
+  resultsPanel.classList.add('hide');
+  gameSelector.classList.remove('hide');
+  uncheckAll();
+}
+
+function uncheckAll() {
+  plusSelect.checked = false;
+  takeAwaySelect.checked = false;
+  friendsOfTenSelect.checked = false;
+  easySelect.checked = false;
+  mediumSelect.checked = false;
+  hardSelect.checked = false;
 }
